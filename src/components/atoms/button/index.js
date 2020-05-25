bdda50d6a65ec982d4d0a113a88579083740b7fb
anggasayogo <anggasayogosm@gmail.com>
2020-05-25 14:30:20 +0700

    refactoring color with global variabels

--- src/components/atoms/button/index.js
+++ src/components/atoms/button/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { colors } from '../../../utils'
+import { color } from 'react-native-reanimated'
 
 const Buttons = ({type,title,onPress}) => {
     return (
@@ -15,14 +17,14 @@ const styles = StyleSheet.create({
     container: (type) => ({
         paddingVertical: 10,
         borderRadius: 10,
-        backgroundColor: type === 'secondary' ? 'white' : '#0BCAD4'
+        backgroundColor: type === 'secondary' ? colors.button.secondary.background : colors.button.primary.background
     }),
     text: (type) => (
         {
             fontSize: 18,
             fontWeight: '600',
             textAlign: 'center',
-            color: type === 'secondary' ? 'black' : 'white',
+            color: type === 'secondary' ? colors.button.secondary.text : colors.button.primary.text,
             fontFamily: 'Nunito-SemiBold'
         }
     )
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Header, Input, Buttons } from '../../components';
 
 const Register = () => {
     return (
-        <View>
-            <Text>Hello Ini Screeen Register</Text>
+        <View style={styles.page}>
+            <Header/>
+            <View style={styles.content}>
+                <Input label="Full Name"/>
+                <Input label="Pekerjaan"/>
+                <Input label="Email"/>
+                <Input label="Password"/>
+                <Buttons title="continue"/>
+            </View>
         </View>
     )
 }
 
 export default Register;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    page: {
+        backgroundColor: 'white',
+        flex: 1
+    },
+    content: {
+        padding: 40,
+        paddingTop: 0
+    }
+})
